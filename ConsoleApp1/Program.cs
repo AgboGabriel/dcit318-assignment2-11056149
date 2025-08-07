6d819704249e538b34a7349f7da1c031ef613dda
GabIsHereAgain <gagbo005@st.ug.edu.gh>
2025-08-07 22:58:29 +0000

    InHeritance and Overriding Program

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,44 @@
+﻿
+using System;
+
+
+
+namespace Inheritance_and_Method_Overriding;
+
+    class program
+    {
+
+        public class Animal()
+        {
+            public string Makesound()
+            {
+                return "some generic sound";
+            }
+        }
+             class Dog(): Animal
+             {
+                public string Makesound()
+                {
+                   return "bark";
+                }
+             }
+               class Cat() : Animal
+               {
+                 public string Makesound()
+                   {
+                            return "Meow";
+                    }
+               }
+            public static void Main(String[] args)
+                {
+                      Animal animal = new Animal();
+                      Dog dog = new Dog();
+                      Cat cat = new Cat();
+                      Console.WriteLine(animal.Makesound());
+                      Console.WriteLine(cat.Makesound());
+                      Console.WriteLine(dog.Makesound());
+                  }
+
+}
+
+
