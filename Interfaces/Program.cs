f0457d60b109e355d6bb1abb811958501081d629
GabIsHereAgain <gagbo005@st.ug.edu.gh>
2025-08-08 16:29:49 +0000

    Interfaces done

---
+++ Interfaces/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+
+namespace Interfaces;
+
+public class program 
+{
+    public interface IMovable
+    {
+        public string move();
+    }
+    public class Car : IMovable
+    {
+        public string move() 
+        {
+         return "Car is moving";
+        }
+    }
+    public class Bicycle : IMovable 
+    {
+        public string move() 
+        {
+         return "Bicycle is moving";
+        }
+    }
+    public static void Main(String[] args) 
+    {
+        Car car = new Car();
+        Bicycle bicycle = new Bicycle();
+        Console.WriteLine(car.move());
+        Console.WriteLine(bicycle.move());
+    }
+}
